7fc4a222773488630451e8c8c9f08e4bd36f62a0
moalam7 <mohammedalamx@gmail.com>
2024-05-06 09:20:08 -0400

    initial_commit

---
+++ demos/intro_to_objects.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//Properties are written as name and value pairs that are separated by a colon
+let emp = {
+  employeeId: "1",
+  name: "Ezra",
+  jobTitle: "Theater Teacher",
+  payRate: 38.46,
+};
+
+//spacing, indentation, and line breaks are a matter of preference
+let emp2 = { employeeId:"1", name:"Ezra", jobTitle:"Theater Teacher", payRate:38.46};
+
+console.log(emp);
+console.log(emp2);
