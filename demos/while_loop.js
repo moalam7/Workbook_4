870aa56d34b61463ebfcbcd128c00415eed7a875
moalam7 <mohammedalamx@gmail.com>
2024-05-07 09:15:17 -0400

    while_loop

---
+++ demos/while_loop.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let number = 1;
+let count = 1;
+
+//while(condition){
+// run the lines of code in here
+// this one too
+// increment the counter, add one to the counter so the condition is eventually met and the loop ends
+//}
+while (count < 5) {
+  number = number * 2;
+  console.log(number);
+  count = count + 1;
+}
