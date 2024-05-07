556014951778976e8106975af5a5de8b2b7ea71f
moalam7 <mohammedalamx@gmail.com>
2024-05-07 14:29:05 -0400

    arrays

---
+++ demos/num_array.js
@@ -0,0 +1,15 @@
+// an array that stores all dates
+let importantDates = [
+  new Date(1958, 8, 5),
+  new Date(1976, 4, 30),
+  new Date(2009, 9, 10)
+  ];
+
+ // an array that stores objects
+ let menu = [
+  {item: "Hamburger", price: 6.95},
+  {item: "Cheeseburger", price: 7.95},
+  {item: "Hot dog", price: 4.95}
+  ];
+
+  let lunch = ["Steak fajitas", 9.95, "Sweet Tea", 2.79];
