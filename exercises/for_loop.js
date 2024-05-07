b1e3777d8c986eb43257d97f3f643f189e83c704
moalam7 <mohammedalamx@gmail.com>
2024-05-07 09:45:00 -0400

    loops

---
+++ exercises/for_loop.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// let num = 1;
+
+// for (let i = 0; i < 5; i++) {
+// num = num * 2;
+// console.log(num);
+// }
+
+
+for (let i=0;i<7;i++){
+  console.log("I love loops!");
+}
---
+++ exercises/while_loops.js
@@ -0,0 +1,19 @@
+"use strict";
+// let num = 1;
+// let i = 1;
+
+// while (i < 100) {
+//  num = num * 2;
+
+
+// if (num >= 100) break;
+//  i++;
+// }
+
+
+let i = 1;
+
+while (i<8){
+  console.log("I love loops!");
+  i++;
+}
