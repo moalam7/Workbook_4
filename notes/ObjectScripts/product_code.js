1ef822743942364473646e6a11668c0da3404c10
moalam7 <mohammedalamx@gmail.com>
2024-05-15 11:36:41 -0400

    product_code

--- notes/ObjectScripts/product_code.js
+++ notes/ObjectScripts/product_code.js
@@ -1,5 +1,44 @@
 "use strict";
 
-function parsePartCode(productCode){
-
+// content before :
+function getSupplierCode(partCode) {
+  const positionOfColon = partCode.indexOf(":");
+  return partCode.substring(0, positionOfColon);
 }
+
+//content between : and -
+function getProductNumber(partCode) {
+  const positionOfColon = partCode.indexOf(":");
+  const positionOfHyphen = partCode.indexOf("-");
+  return partCode.substring(positionOfColon + 1, positionOfHyphen);
+}
+
+//content after -
+function getSize(partCode) {
+  const positionOfHyphen = partCode.indexOf("-");
+  return partCode.substring(positionOfHyphen + 1);
+}
+
+function parsePartCode(partCode) {
+    //call other functions to recieve info
+    let supplierCode = getSupplierCode(partCode);
+    let productNumber = getProductNumber(partCode);
+    let size = getSize(partCode);
+    //define object from this information
+    let partCodeObject = {
+        supplierCode: supplierCode,
+        productNumber: productNumber,
+        size: size
+    }
+    return partCodeObject;
+}
+
+const partCode1 = "ACME:123-L";
+const partCode2 = "DI:12345-M";
+const partCode3 = "ACE:1-12";
+
+let returnedObject = parsePartCode(partCode1);
+console.log(`
+Supplier: ${returnedObject.supplierCode}
+Product Number: ${returnedObject.productNumber}
+Size: ${returnedObject.size}`);
